78abdb2f5484b1f4586c85cb4e36755b4f574cfb
Mohammad-Daaboul98 <mohammad.daaboul.99@gmail.com>
2023-03-21 12:17:56 +0200

    add cros and set mongo to false

--- server.js
+++ server.js
@@ -5,31 +5,29 @@ var cors = require('cors')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
-// express app
 const app = express()
 
-// middleware
 app.use(express.json())
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
+    console.log(req.path, req.method);
+    next()
 })
-
 app.use(cors())
 
-// routes
+//route
 app.use('/api/workouts', workoutRoutes)
 
-// connect to db
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('connected to database')
-    // listen to port
-    app.listen(process.env.PORT, () => {
-      console.log('listening for requests on port', process.env.PORT)
+//connect db
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONG_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port', process.env.PORT)
+        })
     })
-  })
-  .catch((err) => {
-    console.log(err)
-  }) 
+    .catch(e => {
+        console.log(e);
+    })
+
